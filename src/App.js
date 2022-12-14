b71828b6af2d2148d77d6b41951becfb5ffcfb71
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-14 19:15:13 +0430

    add addbook component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/self-closing-comp */
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
---
+++ src/components/homePage.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+// import BookList from './bookList';
+import AddBook from './addBook';
+
+const homePage = () => (
+  <div className="homePage">
+    {/* <BookList /> */}
+    <AddBook />
+  </div>
+);
+
+export default homePage;
