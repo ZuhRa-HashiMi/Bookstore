2b5a7925285807e84cb328c74321528897b9c826
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-15 14:26:01 +0430

    delete the profile part

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const navBar = () => (
   <div className="navBar">
     <h1>Book Store</h1>
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
-    </Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/categories">Categories</Link>
+        </li>
+      </ul>
+    </nav>
   </div>
 );
 
