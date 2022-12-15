737928c811c55d36ea74f620f3f5f5368ff22426
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-15 14:24:52 +0430

    modified app.js'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/navBar';
 import HomePage from './components/homePage';
 import Categories from './components/categories';
-import Footer from './components/footer';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <NavBar />
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/categories" element={<Categories />} />
+//         </Routes>
+//       </BrowserRouter>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
