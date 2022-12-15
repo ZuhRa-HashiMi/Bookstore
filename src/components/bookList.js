61bfed60463772c5c8ff867cb794b2a5f71918ac
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-15 10:52:32 +0430

    delete the ul from bookList function

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -3,11 +3,9 @@ import SingleBook from './singleBook';
 
 const BookList = () => (
   <div className="BookList">
-    <ul>
-      <SingleBook title="Alice In Wonderland" author="Lewis Carroll" />
-      <SingleBook title="Harry Porter" author="JK Rowlings" />
-      <SingleBook title="The Hobbit" author="JRR Tolkien" />
-    </ul>
+    <SingleBook title="Alice In Wonderland" author="Lewis Carroll" />
+    <SingleBook title="Harry Porter" author="JK Rowlings" />
+    <SingleBook title="The Hobbit" author="JRR Tolkien" />
   </div>
 );
 
