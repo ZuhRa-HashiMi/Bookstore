994a6b7071c0ab3a9100ed0e3160de9f6bd8088a
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-04 13:19:17 +0430

    add Navigation component

--- src/components/categories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Categories() {
-  return (
-    <div className="categories">
-      <button type="button">Check Status</button>
-    </div>
-  );
-}
