2997eb3c0e90fec0a878e536576e3f626c239f1b
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-04 18:18:42 +0430

    solve the linter errors

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-
-import { addBook } from '../redux/books/books';
+import { addBook } from '../redux/books/book';
 
 const AddBook = () => {
   const [book, setBook] = useState({});
