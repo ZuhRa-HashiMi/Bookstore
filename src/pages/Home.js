fcb7d703f7d6653ad29fcb483feb893a1913fb7e
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-04 13:28:50 +0430

    add book component and AddBook component

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+
+const Home = () => {
+    const books = useSelector((state) => state.books);
+  
+    return (
+      <>
+        {books.map((book) => (
+          <div className="container" key={book.id}>
+            <Book title={book.title} author={book.author} id={book.id} />
+          </div>
+        ))}
+        <AddBook />
+      </>
+    );
+  };
+  
+  export default Home;
