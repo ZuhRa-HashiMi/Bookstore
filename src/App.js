1c5676b4470aca8fb091bf168d608403242857e6
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-04 22:48:06 +0430

    add provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+
 import Navigation from './components/Navigation';
 import Categories from './pages/Categories';
 import Home from './pages/Home';
+import store from './redux/configureStore';
 
 const App = () => (
-  <>
+  <Provider store={store}>
     <Navigation />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="categories" element={<Categories />} />
     </Routes>
-  </>
+  </Provider>
 );
 
 export default App;
