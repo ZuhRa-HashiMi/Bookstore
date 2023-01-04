10e74acaf9cc052c5d9dc54332e4d06cd313b9af
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-04 15:29:35 +0430

    solved the linter errror

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import AddBook from '../components/AddBook.js';
+import AddBook from '../components/AddBook';
 import Book from '../components/Book';
 
 const Home = () => {
