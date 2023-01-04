cccf271958dc1ec760171591912dfdfeee7b9170
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-04 15:05:34 +0430

    bring some changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,20 @@
 import { useSelector } from 'react-redux';
 import AddBook from '../components/AddBook';
 import Book from '../components/Book';
+
 const Home = () => {
-    const books = useSelector((state) => state.books);
-  
-    return (
-      <>
-        {books.map((book) => (
-          <div className="container" key={book.id}>
-            <Book title={book.title} author={book.author} id={book.id} />
-          </div>
-        ))}
-        <AddBook />
-      </>
-    );
-  };
-  
-  export default Home;
+  const books = useSelector((state) => state.books);
+
+  return (
+    <>
+      {books.map((book) => (
+        <div className="container" key={book.id}>
+          <Book title={book.title} author={book.author} id={book.id} />
+        </div>
+      ))}
+      <AddBook />
+    </>
+  );
+};
+
+export default Home;
--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -3,25 +3,24 @@ const REMOVE_BOOK = 'book-store/books/REMOVE_BOOK';
 
 const books = [];
 const BookReducer = (state = books, action) => {
-    switch (action.type) {
-      case ADD_BOOK:
-        return [...state, action.payload];
-      case REMOVE_BOOK:
-        return state.filter((book) => book.id !== action.id);
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
+};
 
-  
 export const addBook = (payload) => ({
-    type: ADD_BOOK,
-    payload,
-  });
-  
-  export const removeBook = (payload) => ({
-    type: REMOVE_BOOK,
-    id: payload,
-  });
+  type: ADD_BOOK,
+  payload,
+});
 
-  export default BookReducer;
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  id: payload,
+});
+
+export default BookReducer;
