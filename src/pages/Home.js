f6df22c6ef85f4dc26f71b2cde672d97adfcf269
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-05 16:57:53 +0430

    add categoery object to Home coomponent

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import AddBook from '../components/AddBook';
 import Book from '../components/Book';
+import { loadBooks } from '../redux/books/book';
 
 const Home = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadBooks());
+  }, [dispatch]);
 
   return (
     <>
-      {books.map((book) => (
-        <div className="container" key={book.id}>
-          <Book title={book.title} author={book.author} id={book.id} />
-        </div>
+      {Object.keys(books).map((id) => (
+        <Book
+          key={id}
+          id={id}
+          title={books[id][0].title}
+          author={books[id][0].author}
+          category={books[id][0].category}
+        />
       ))}
       <AddBook />
     </>
